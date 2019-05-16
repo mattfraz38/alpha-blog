9630b2c6630a1383bda33044aa1cb13d3cf9c3ea
Matt Frazier <frazier3388@yahoo.com>
2019-05-16 22:12:37 +0900

    Add log in and log out session and favicon

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+  def new
+
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:success] = "Welcome back #{user.username}"
+      redirect_to user_path(user)
+    else
+      flash.now[:danger] = "There was something wrong with your login information"
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "Come back again soon, bye"
+    redirect_to root_path
+  end
+end
