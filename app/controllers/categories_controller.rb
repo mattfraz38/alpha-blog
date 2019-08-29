4070c5254143d93b439b34ef5e81f5b5ab68b927
Matt Frazier <frazier3388@yahoo.com>
2019-08-30 02:21:49 +0900

    add categories integration test

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,29 @@
 class CategoriesController < ApplicationController
 
   def index
-  end
-
-  def new
+    @categories = Category.all
   end
 
   def show
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:success] = "#{@category.name} was added!"
+      redirect_to categories_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
